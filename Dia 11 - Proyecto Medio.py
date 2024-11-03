4abd66c2e494357a918d4eb7d9f577cd9d64558b
lsantucho26 <lsantucho2629@gmail.com>
2024-11-03 19:37:28 -0300

    Fecha03/11

---
+++ Dia 11 - Proyecto Medio.py
@@ -0,0 +1,114 @@
+#Declaramos variables
+stamp: str();
+tablet: str();
+clock: str();
+sofa: str();
+boots: str();
+computer: str();
+alarm_clock: str(); 
+watch: str();
+traffic_lights: str();
+digital: str();
+watch: str();
+books: str();
+cuckoo_clok: str();
+
+#Inicializamos varialbes
+stamp= "stamp";
+tablet= "tablet";
+clock= "clock";
+sofa= "sofa";
+boots= "boots";
+computer= "computer";
+alarm_clock= "alarm clock";
+watch= "watch";
+traffic_lights= "traffic lights";
+digital= "digital";
+watch= "watch";
+books= "books";
+cuckoo_clock= "cuckoo clock";
+
+#importamos librerias
+import os;
+import time;
+
+#Comenzamos
+os.system("cls");
+
+print("Welcome to Wall Street English! A continuacion podras reforzar tus conocimiento en Ingles completando algunas palabras del idioma. ");
+print("Te presentaremos algunos objetos, deberas escribir su nombre en Ingles y al acertar podras continuar a las siguiente palabras.");
+
+time.sleep(8);
+os.system("cls");
+
+#Presentamos los ejercicios
+a = input("Escribe el nombre de 👢👢: ");
+while a != boots:
+    a = input ("Escribe tu palabra nuevamente: ");
+
+print("¡Muy bien!");
+time.sleep(5);
+os.system("cls");
+
+
+b = input("A continuacion escribe el nombre de ⌚: ");
+while b != watch:
+    b = input ("Escribe tu palabra nuevamente: ");
+
+print("¡Muy bien!");
+time.sleep(5);
+os.system("cls");
+
+
+c = input("A continuacion escribe el nombre de 📚: ");
+while c != books:
+    c = input ("Escribe tu palabra nuevamente: ");
+
+print("¡Muy bien!");
+time.sleep(5);
+os.system("cls");
+
+
+d = input("A continuacion escribe el nombre de 🚦: ");
+while d != traffic_lights:
+    d = input ("Escribe tu palabra nuevamente: ");
+
+print("¡Muy bien!");
+time.sleep(5);
+os.system("cls");
+
+
+e = input("A continuacion escribe el nombre de ⏰: ");
+while e != alarm_clock:
+    e = input ("Escribe tu palabra nuevamente: ");
+
+print("¡Muy bien!");
+time.sleep(5);
+os.system("cls");
+
+
+f = input("A continuacion escribe el nombre de 🛋️: ");
+while f != sofa:
+    f = input ("Escribe tu palabra nuevamente: ");
+
+print("¡Muy bien!");
+time.sleep(5);
+os.system("cls");
+
+
+g = input("A continuacion escribe el nombre de 🖥️🖲️: ");
+while g != computer:
+    g = input ("Escribe tu palabra nuevamente: ");
+
+print("¡Muy bien!");
+time.sleep(5);
+os.system("cls");
+
+
+h = input("A continuacion escribe el nombre de 🕝: ");
+while h != clock:
+    h = input ("Escribe tu palabra nuevamente: ");
+
+print("¡Muy bien!");
+time.sleep(5);
+os.system("cls");
